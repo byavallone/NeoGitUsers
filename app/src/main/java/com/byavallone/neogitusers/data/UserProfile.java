bc990d7134b5a67595ff49d8352a43184b65780b
Eduardo Avallone de Souza <edu.ava@gmail.com>
2018-09-03 00:25:04 -0300

    Adding extra fields on UserProfile object

--- app/src/main/java/com/byavallone/neogitusers/data/UserProfile.java
+++ app/src/main/java/com/byavallone/neogitusers/data/UserProfile.java
@@ -9,6 +9,8 @@ public class UserProfile {
     private String mRealName;
     private String mCompany;
     private String mLocation;
+    private String mImageUrl;
+    private String mProfileWebUrl;
     private int mRepoCount;
     private int mFollowers;
 
@@ -17,9 +19,11 @@ public class UserProfile {
      * @param nickName
      * @param followers
      */
-    UserProfile(String nickName, int followers){
+    UserProfile(String nickName, int followers, String imageURL, String profileWebUrl){
         mNickName = nickName;
         mFollowers = followers;
+        mImageUrl = imageURL;
+        mProfileWebUrl = profileWebUrl;
     }
 
     /** Method used to get the nickName of the user */
@@ -31,4 +35,10 @@ public class UserProfile {
     public int getFollowers(){
         return mFollowers;
     }
+
+    /** Method used to get the image url */
+    public String getImageUrl(){return mImageUrl;}
+
+    /** Method used to get the profile web url */
+    public String getProfileWebUrl(){return mProfileWebUrl;}
 }
